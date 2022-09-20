0d8d2f9ab7977c69c0e83ad523f10fa699ffb59a
pauloemil <prog.paulo@hotmail.com>
2022-09-20 15:12:11 +0200

    Edit the test cases to fit the refactor

--- src/test/java/com/example/demo/Services/ActionServiceTest.java
+++ src/test/java/com/example/demo/Services/ActionServiceTest.java
@@ -1,7 +1,11 @@
 package com.example.demo.Services;
 
-import com.example.demo.Entities.Action;
+import com.example.demo.DTOs.ActionRequestDto;
+import com.example.demo.DTOs.ParameterDto;
+import com.example.demo.Entities.*;
 import com.example.demo.Exceptions.NotFoundException;
+import com.example.demo.Mappers.ActionMapper;
+import com.example.demo.Mappers.ParameterMapper;
 import com.example.demo.Repositories.ActionRepository;
 import com.example.demo.ServicesImplementations.ActionServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +14,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
@@ -18,22 +23,42 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
 @ExtendWith(SpringExtension.class)
+@SpringBootTest
 class ActionServiceTest {
 	@MockBean
 	private ActionRepository actionRepository;// = mock(ActionRepository.class);
+	@MockBean
+	private ParameterService parameterService;
+	@MockBean
+	private ActionTypeService actionTypeService;
+	@MockBean
+	private ApplicationService applicationService;
+	@MockBean
+	private BusinessEntityService businessEntityService;
+	@MockBean
+	private UserService userService;
+	@MockBean
+	private ParameterTypeService parameterTypeService;
+
 	@Autowired
 	private ActionService actionService;
+
+	@Autowired
+	ActionMapper actionMapper;
+	@Autowired
+	ParameterMapper parameterMapper;
 
 	private static final long SOME_ID = 123;
 	private static final long SOME_WRONG_ID = 321;
@@ -48,10 +73,59 @@ class ActionServiceTest {
 	}
 
 	Action action;
+	ActionRequestDto actionRequestDto;
+	ActionType actionType;
+	BusinessEntity be;
+	Application application;
+	User user;
+	ParameterDto customerParameter;
+	List<ParameterDto> parameters;
 	@BeforeEach
 	void setUp() {
 		action = new Action();
 		action.setAction_id(SOME_ID);
+		System.out.println(action);
+
+		parameters = new ArrayList<>();
+
+		customerParameter = new ParameterDto();
+		customerParameter.setParameter_id(12);
+		customerParameter.setParameter_type_name("customer");
+		customerParameter.setParameter_value("paulo");
+		parameters.add(customerParameter);
+
+		ParameterDto orderParameter = new ParameterDto();
+		orderParameter.setParameter_id(11);
+		orderParameter.setParameter_type_name("order");
+		orderParameter.setParameter_value("Paulo's order");
+		parameters.add(orderParameter);
+
+		ParameterDto productParameter = new ParameterDto();
+		productParameter.setParameter_id(11);
+		productParameter.setParameter_type_name("product");
+		productParameter.setParameter_value("Wrak fra5 bel bsal el a7mar");
+		parameters.add(productParameter);
+
+		actionRequestDto = new ActionRequestDto();
+		actionRequestDto.setParameters(parameters);
+		actionRequestDto.setAction_type_name("ORDER_CREATED");
+		actionRequestDto.setUser_id(1);
+		actionRequestDto.setApplication_id(1);
+		actionRequestDto.setBusiness_entity_id(1);
+		actionRequestDto.setBusiness_entity_id(1);
+
+		actionType = new ActionType();
+		actionType.setMessage_template_en("{{customer.value}} bomba");
+		actionType.setMessage_template_ar("{{customer.value}} بومبا");
+
+		be = new BusinessEntity();
+		be.setBusiness_entity_name("BE Name");
+
+		application = new Application();
+		application.setApplication_name("Application Name");
+
+		user = new User();
+		user.setUser_name("User Name");
 	}
 
 	@Test
@@ -84,10 +158,37 @@ class ActionServiceTest {
 	@Test
 	void saveValidAction_shouldReturnIt() {
 		when(actionRepository.save(any(Action.class))).then(answer -> answer.getArgument(0));
+		when(parameterService.saveParameter(any(Parameter.class))).then(answer -> answer.getArgument(0));
+		when(businessEntityService.findBusinessEntityById(anyLong())).then(
+				answer -> {
+					be.setBusiness_entity_id(answer.getArgument(0));
+					return be;
+				}
+		);
+		when(applicationService.findApplicationById(anyLong())).then(
+				answer -> {
+					application.setApplication_id(answer.getArgument(0));
+					return application;
+				}
+		);
+		when(userService.findUserById(anyLong())).then(
+				answer -> {
+					user.setUser_id(answer.getArgument(0));
+					return user;
+				}
+		);
+		when(actionTypeService.findActionTypeByName(anyString())).then(
+				answer -> {
+					actionType.setName(answer.getArgument(0));
+					return actionType;
+				}
+		);
+		Action saved = actionService.saveAction(actionRequestDto);
 
-		Action saved = actionService.saveAction(action);
+		assertThat(saved.getUser().getUser_name()).isEqualTo(user.getUser_name());
+		assertThat(saved.getBusiness_entity().getBusiness_entity_name()).isEqualTo(be.getBusiness_entity_name());
+		assertThat(saved.getAction_type().getName()).isEqualTo(actionType.getName());
+		assertThat(saved.getDescription_en()).contains(customerParameter.getParameter_value());
 
-		assertThat(saved.getAction_id()).isEqualTo(SOME_ID);
-		verify(actionRepository).save(action);
 	}
 }
