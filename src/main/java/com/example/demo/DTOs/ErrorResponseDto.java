c90be70089ac2158d4d791c6cc987fc0c8015cba
pauloemil <prog.paulo@hotmail.com>
2022-09-20 14:10:50 +0200

    Delete cause no usage

--- src/main/java/com/example/demo/DTOs/ErrorResponseDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.DTOs;
-
-import lombok.Data;
-import org.springframework.messaging.support.ErrorMessage;
-
-@Data
-public class ErrorResponseDto {
-	private String errorMessage;
-}
